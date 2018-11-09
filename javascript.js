e6603af0811679a880806d5e015064e8c3346650
spilmont <43751712+spilmont@users.noreply.github.com>
2018-11-09 10:02:19 +0100

    Generateur Histoire (andré brian) -tableau

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+var Noms= ["Michel Varuk","Jim Carrey","Albert Einstein","Vivaldi","Emma Watson","Renaud"
+
+];
+var Objets=[
+
+];
+var Temperatures=[
+
+];
+var Lieux=[
+
+];
+var Verbes=[
+
+];
