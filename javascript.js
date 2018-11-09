21cc45d0e10b8a9d6dc22a5822a05b8fde4f36aa
spilmont <43751712+spilmont@users.noreply.github.com>
2018-11-09 10:19:37 +0100

    Generateur Histoire (andré brian) -tableau

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 
-var Noms= ["Michel Varuk","Jim Carrey","Albert Einstein","Vivaldi","Emma Watson","Renaud"
-
+var Noms= ["Michel Varuk","Jim Carrey","Albert Einstein","Vivaldi","Emma Watson","Renaud","Eminem","Paul Ludd","Jacques",
+            "Bernadette","Johnny Depp","The Rock","Billy Obam","Ciryle Vin","Léa","Nina","Julien Lepers","Hugh Jackman",
+            "jacquie","Michel","GOTAGA","Philippe Etchebest","H.P LofeCraft","Bill Gate"
 ];
 var Objets=[
 
