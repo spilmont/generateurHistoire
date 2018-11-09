0aeb2de0dffb1f6e0b2eef1a67b816d6cf68a6f4
spilmont <43751712+spilmont@users.noreply.github.com>
2018-11-09 11:17:16 +0100

    Generateur Histoire (andré brian) -tableau

--- javascript.js
+++ javascript.js
@@ -3,14 +3,15 @@ var Noms= ["Michel Varuk","Jim Carrey","Albert Einstein","Vivaldi","Emma Watson"
             "Bernadette","Johnny Depp","The Rock","Billy Obam","Ciryle Vin","Léa","Nina","Julien Lepers","Hugh Jackman",
             "jacquie","Michel","GOTAGA","Philippe Etchebest","H.P LofeCraft","Bill Gate"
 ];
-var Objets=[
+var Objets=["ordinateur","telephone","smartphone","montre","dé a coudre","decapsuleur","ballon de foot","clavier","marteau",
+            "tasse","piece","lunette de soleil","sabre","cuilliere","fourchette","manette","avion","miche de pain","vélo"
 
 ];
-var Temperatures=[
-
+var Temperatures=["10°C","30°C","37,2°C","100°F","50°C","16°C","12°C","25°C","22°C","69°C","18°C","37°C","70°C","5°C","28°C"
 ];
-var Lieux=[
-
+var Lieux=["Floyon","Wignehies","Barcelone","Paris","Mons","Le Touquet","Fourmies","Big Ben","Tokyo","Reykjavik","Bondy",
+            "Rio de Janero","Mont Fuji","Palais de l'Elysée","Mairie de Paris","Macdo","KFC","Burger King","Moncuq",
+            "chute du Niagara","Hide Park"
 ];
 var Verbes=["code","copie","coordonne","nage","monte","a","aime","est","se passionne","developpe","joue","s'ouleve","evolue",
             "vote","arnaque","Mange","s'envoye","enfourne","acomplit","comunique"
